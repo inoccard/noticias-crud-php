1edc40abfc1d2e9f7062b080fb1377ae5bb696c4
inoccard <inocenciocardoso19@gmail.com>
2018-11-13 13:28:10 -0200

    rodapé

--- views/_includes/footer.php
+++ views/_includes/footer.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('ABSPATH')) exit; ?>
 <footer>
-    <div>CopyRight &copy2018 MuTech</div>
+    <div>CopyRight &copy2018 67 Tech</div>
 </footer>
 </main> <!-- .main-page (header.php) -->
 </body>
