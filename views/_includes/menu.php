73b4bb33e91d5bba1ea98777ba57981c20ac8cdd
inoccard <inocenciocardoso19@gmail.com>
2018-11-20 20:57:51 -0200

    menu

--- views/_includes/menu.php
+++ views/_includes/menu.php
@@ -4,11 +4,12 @@
 
 <nav class="menu clearfix">
 	<ul>
-		<li><a href="<?php echo HOME_URI;?>">Home</a></li>
-		<li><a href="<?php echo HOME_URI;?>/login/">Login</a></li>
-		<li><a href="<?php echo HOME_URI;?>/user-register/">User Register</a></li>
-		<li><a href="<?php echo HOME_URI;?>/noticias/">Notícias</a></li>
-		<li><a href="<?php echo HOME_URI;?>/noticias/adm/">Notícias Admin</a></li>
 		<li><a href="<?php echo HOME_URI;?>/exemplo/">Exemplo básico</a></li>
+		<li><a href="<?php echo HOME_URI;?>/noticias/adm/">Notícias Admin</a></li>
+		<li><a href="<?php echo HOME_URI;?>/noticias/">Notícias</a></li>
+		<li><a href="<?php echo HOME_URI;?>/user-register/">User Register</a></li>
+		<li><a href="<?php echo HOME_URI;?>/login/">Login</a></li>
+		<li><a href="<?php echo HOME_URI;?>">Home</a></li>
+		
 	</ul>
 </nav>
